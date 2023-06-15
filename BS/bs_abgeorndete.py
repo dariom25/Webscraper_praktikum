49d24cdeb5ecc61d26b380d0674227eab3c3b8f1
dariom25 <dario@maciey.de>
2023-06-15 16:01:09 +0200

    Restructure code into class with methods

--- BS/bs_abgeorndete.py
+++ BS/bs_abgeorndete.py
@@ -1,50 +1,53 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 
+class Scraper:
+    names = []
+    parties = []
+    email = []
 
-names = []
-parties = []
-email = []
+    def __init__():
+        source = requests.get("https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_Deutschen_Bundestages_(20._Wahlperiode)").text
+        soup = BeautifulSoup(source, "lxml")
+        tables = soup.find_all("table")
+        counter = 0
+        
+        for table in tables:
+            counter += 1
+            if counter == 3:
+                politicians = table.find_all("tr")
+                for person in politicians:
+                    data = person.find_all("td")
+                    scnd_counter = 0
+                    for entry in data:
+                        if scnd_counter == 1:
+                            name = entry.text
+                            name = name.strip("\n")   
+                            Scraper.names.append(name)
+                        elif scnd_counter == 3:
+                            party = entry.text
+                            party = party.strip("\n")
+                            Scraper.parties.append(party)
+                        scnd_counter += 1
 
-def parse_name_and_party_from_html_code():
+    def export_parties_to_txt(self, parties):
+        with open("bundestagsparteien.txt", "w", encoding="UTF-8") as f:
+            f.write("Partei" + "\n")
+            for partei in parties:
+                f.write(partei + ",\n")
+
+    def export_names_to_txt(self, names):
+        with open("bundestagsabgeordnete.txt", "w", encoding="UTF-8") as f:
+            f.write("Name" + "\n")
+            for name in names:
+                f.write(name + ",\n")
     
-    source = requests.get("https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_Deutschen_Bundestages_(20._Wahlperiode)").text
+    def split_names_into_sur_and_last_name(self, names):
+        pass
 
-    soup = BeautifulSoup(source, "lxml")
-
-    tables = soup.find_all("table")
-    counter = 0
-    for table in tables:
-        counter += 1
-        if counter == 3:
-            politicians = table.find_all("tr")
-            for person in politicians:
-                data = person.find_all("td")
-                scnd_counter = 0
-                for entry in data:
-                    if scnd_counter == 1:
-                        name = entry.text
-                        name = name.strip("\n")   
-                        names.append(name)
-                    elif scnd_counter == 3:
-                        party = entry.text
-                        party = party.strip("\n")
-                        parties.append(party)
-                    scnd_counter += 1
-
-def export_data_to_txt(parties, names):
-    with open("bundestagsparteien.txt", "w", encoding="UTF-8") as f:
-        f.write("Partei" + "\n")
-        for partei in parties:
-            f.write(partei + ",\n")
-
-    with open("bundestagsabgeordnete.txt", "w", encoding="UTF-8") as f:
-        f.write("Name" + "\n")
-        for name in names:
-            f.write(name + ",\n")
-
-
+name_party_mail_df = pd.DataFrame({"Name": })
 
 
 
