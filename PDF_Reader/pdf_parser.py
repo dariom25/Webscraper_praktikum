71eac3208327d2d1f06f903c4d4a64f266cdec46
dariom25 <dario@maciey.de>
2023-06-23 09:11:43 +0200

    Change the parser to a class structure

--- PDF_Reader/pdf_parser.py
+++ PDF_Reader/pdf_parser.py
@@ -1,15 +1,31 @@
 from PyPDF2 import PdfReader
 
-abgeordnete = []
+class Pdfparser:
 
-reader = PdfReader("Abgeordnetenportraits_RLP.pdf")
-number_of_pages = len(reader.pages)
+    abgeordnete = []
+    emails = []
 
-for site in range(118):
-    pass
+    pdf_name = "Abgeordnetenportraits_RLP.pdf"
 
-page = reader.pages[21]
-text = page.extract_text()
-abgeordnete.append(text)
-abgeordnete = abgeordnete[0].split("\n")
-print(abgeordnete)
+    def read_pdf(self, pdf_name):
+        reader = PdfReader(pdf_name)
+        number_of_pages = len(reader.pages)
+
+        for site in range(118):
+            pass
+
+        page = reader.pages[21]
+        text = page.extract_text()
+        self.abgeordnete.append(text)
+        self.abgeordnete = self.abgeordnete[0].split("\n")
+
+    def get_emailadress(self, liste_von_abgeordneten):
+        for element in liste_von_abgeordneten:
+            if element.find("Email") != -1:
+                self.emails.append(element)
+
+if __name__ == "__main__":
+    pdfparser = Pdfparser()
+    pdfparser.read_pdf(pdfparser.pdf_name)
+    pdfparser.get_emailadress(pdfparser.emails)
+    print(pdfparser.emails)
