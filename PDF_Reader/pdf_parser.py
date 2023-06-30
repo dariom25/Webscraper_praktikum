df00b5c124db4a0c2eddd1d591feaf117128ac13
dariom25 <dario@maciey.de>
2023-06-30 14:11:11 +0200

    Fix bugs in the read_pdf and get_emailadress func

--- PDF_Reader/pdf_parser.py
+++ PDF_Reader/pdf_parser.py
@@ -16,17 +16,22 @@ class Pdfparser:
 
         page = reader.pages[21]
         text = page.extract_text()
-        self.abgeordnete.append(text)
-        self.abgeordnete = self.abgeordnete[0].split("\n")
+        Pdfparser.abgeordnete.append(text)
+        Pdfparser.abgeordnete = Pdfparser.abgeordnete[0].split("\n")
 
-# ich habe das gefühl er geht hier nicht über die liste drüber, sondern nimmt nur das erste element und ist das so: bye
     def get_emailadress(self, liste_von_abgeordneten):
         for element in liste_von_abgeordneten:
             if element.find("Email") != -1:
-                self.emails.append(element)
+                Pdfparser.emails.append(element)
+                
+    def get_name(self, liste_von_abgeordneten):
+        for element in liste_von_abgeordneten:
+            if element == liste_von_abgeordneten[0]:
+                Pdfparser.abgeordnete.append(element)
+                
 
 if __name__ == "__main__":
     pdfparser = Pdfparser()
     pdfparser.read_pdf(pdfparser.pdf_name)
-    pdfparser.get_emailadress(pdfparser.emails)
-    print(pdfparser.emails)
+    pdfparser.get_emailadress(pdfparser.abgeordnete)
+    print(pdfparser.abgeordnete)
